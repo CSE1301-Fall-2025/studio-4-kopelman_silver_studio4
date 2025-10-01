ccd803a2356726ee54f3a11bb7b0dc8d5faacbb5
Micah Kopelman <kmicah@wustl.edu>
2025-10-01 13:54:54 -0500

    Studio 4 in the books!

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,21 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+
+		//blue background
+		StdDraw.setPenColor(117, 170,219 );
+		StdDraw.filledRectangle(0.5, 0.5, 0.5, 0.5);
+
+		//white lines
+		StdDraw.setPenColor(255,255,255);
+		StdDraw.filledRectangle(0.5, 0.65, 0.5, 0.025);
+
+		StdDraw.setPenColor(255,255,255);
+		StdDraw.filledRectangle(0.5, 0.35, 0.5, 0.025);
+
+		// Black line
+		StdDraw.setPenColor(0,0,0);
+		StdDraw.filledRectangle(0.5, 0.5, 0.5, 0.13);
 		
 	}
 }
